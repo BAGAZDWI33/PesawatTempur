e049ec111663472764aa8cba4b2f81ba7a8e5527
BAGAZDWI33 <bagasdwiprasetyo2801@gmail.com>
2022-10-04 04:22:12 -0700

    menambahkan file lab1_tag_tugas.python

---
+++ lab1_tag_tugas.python.py
@@ -0,0 +1,48 @@
+while True:
+    def nama():
+        print('Pesawat Tempur')
+    nama()
+    PesawatTempur = input('warna pesawat: ')
+    def energi():
+         print ('nyalakan mesin')
+    energi()
+
+    on = input('keadaan mesin:')
+
+    if on == ('on'):
+        print('lanjut')
+        def ketinggian():
+            print ('ketinggian pesawat')
+        ketinggian()
+
+        ketinggian = input('mau kemana anda?')
+
+        if ketinggian == ('terbang'):
+            print('naik')
+        elif ketinggian == ('turun'):
+            print('kebawah')
+        elif ketinggian == ('belok'):
+            print = (input('belok kemana: '))
+
+        belok = input('belok kemana?')
+    
+        if belok == ('kanan'):
+            print('pesawat belok ke kanan')
+        else:
+            print('pesawat belok ke kiri')
+        def kecepatan():
+            print(kecepatan)
+     
+        kecepatan = input('pesawat bergerak dengan: ')
+        Keterangan1 = 'tambah kecepatan'
+        Keterangan2 = 'kurang kecepatan'
+
+        if kecepatan == ('keterangan1'):
+            print('tambah kecepatan')
+        elif kecepatan == ('keterangan2'):
+            print ('kurang kecepatan')
+    else:
+        print ('stop')
+    break
+    
+    
